6d8c8d4a268bd338b8e62c6cb6b473ac0c432108
marcosfeijoo1997 <marcosfeijoo97@gmail.com>
2023-09-18 20:22:53 -0300

    portafolio

---
+++ src/Componentes/header.js
@@ -0,0 +1,14 @@
+import './style.css'
+import Fondo from '../Assets/james-harrison-vpOeXr5wmR4-unsplash.jpg'
+const Header=()=>{
+    return (
+        <div className='presentacion' id='inicio'><div className='fondo'>
+          <div className='oscuro'>
+          <p className='name'>Hola, soy Marcos Feijoo</p>
+          <p className='job-title'>Front-End developer</p>
+          </div>
+          </div>
+        </div>
+      );
+}
+export default Header
